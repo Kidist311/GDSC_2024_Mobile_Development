8f561c787c311d5f30724af11d14625247d557af
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2023-11-28 05:32:11 -0800

    Update Task-2.dart

--- Dart-Task/Task-2.dart
+++ Dart-Task/Task-2.dart
@@ -1,1 +1,26 @@
+void main() {
+List<int> numbers = [12, 34, 67, 55, 89, 23, 8, 92];
+int maxNum = numbers[0];
+int minNum = numbers[0];
+
+  for (int i = 1; i < numbers.length; i++) {
+      if (numbers[i] > maxNum) {
+         maxNum = numbers[i];
+         }
+      if (numbers[i] < minNum) {
+        minNum = numbers[i];
+        }
+  }
+    double sum = 0.0;
+  for (int number in numbers) {
+    sum += number;
+  }
+   double average = sum / numbers.length;
+  
+
+  print("The maximum number is $maxNum");
+  print("The minimum number is $minNum");
+  print("Sum: $sum");
+  print("Average: $average");
+}
 
