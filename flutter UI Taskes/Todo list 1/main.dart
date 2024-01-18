9aebaa7cc83c48a885093d28e28e1231f3fe61ee
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-01-18 04:13:20 -0800

    Update main.dart

--- flutter UI Taskes/Todo list 1/main.dart
+++ flutter UI Taskes/Todo list 1/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/homepage.dart';
+import 'package:todolist/page1.dart';
+import 'package:todolist/page2.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false,
+      home: Todo(),
+ 
+
+   ),
+  );
+}
+ 
