5526f4f10dd881c618a0ad30f38d85a853d3dd5e
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-03-01 11:14:05 -0800

    Update list.dart

--- flutter state magegment task/todolist-1/page 2/list.dart
+++ flutter state magegment task/todolist-1/page 2/list.dart
@@ -1,1 +1,15 @@
+import 'package:uuid/uuid.dart';
 
+const uuid = Uuid();
+
+
+
+class ListOne {
+  ListOne({required this.title,required this.discription})
+      : id = uuid.v4();
+
+  final String title;
+  final String id;
+  final String discription;
+
+}
