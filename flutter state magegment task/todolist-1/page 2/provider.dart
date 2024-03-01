fd0897a97a81eddb809d2762d879d9dc10a2460c
Kidist311 <149496670+Kidist311@users.noreply.github.com>
2024-03-01 11:12:22 -0800

    Update provider.dart

--- flutter state magegment task/todolist-1/page 2/provider.dart
+++ flutter state magegment task/todolist-1/page 2/provider.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'list.dart';
 
+class Project extends ChangeNotifier {
+   DateTime _selectedDate;
+
+  Project({required DateTime initialDate}) : _selectedDate = initialDate ;
+
+  DateTime get selectedDate => _selectedDate;
+
+  void updateSelectedDate(DateTime newDate) {
+    _selectedDate = newDate;
+    notifyListeners();
+  }
+
+List<ListOne> tasks = [];
+List<ListOne> get allTasks => tasks;//to access the whole list
+ 
+  void addTypes(ListOne task) {
+    tasks.add(task);
+    notifyListeners();
+  }
+}
